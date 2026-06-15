dfdc7709e89eaea66d2a269480c7cc8b0c6dfde0
Wangziyi-1207 <whypnos3@gmail.com>
2026-06-15 19:49:10 +0900

    Implement registration and login functions

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,4 +1,25 @@
+async function register() {
+
+    const res = await fetch('/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username.value,
+            password: password.value
+        })
+    });
+
+    if (res.ok) {
+        alert('Register successful');
+    } else {
+        alert('Register failed');
+    }
+}
+
 async function login() {
+
     const res = await fetch('/auth/login', {
         method: 'POST',
         headers: {
@@ -10,9 +31,17 @@ async function login() {
         })
     });
 
+    if (!res.ok) {
+        alert('Username or password incorrect');
+        return;
+    }
+
     const data = await res.json();
 
-    localStorage.setItem('token', data.token);
+    localStorage.setItem(
+        'token',
+        data.token
+    );
 
     location.href = 'dashboard.html';
 }
