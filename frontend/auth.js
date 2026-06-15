6bfbba8f5393df12e49cc1cd793de40db9554bf5
Wangziyi-1207 <whypnos3@gmail.com>
2026-06-15 19:40:00 +0900

    Update login function to use relative URL

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,13 +1,18 @@
 async function login() {
-    const res = await fetch('http://localhost:3000/auth/login', {
+    const res = await fetch('/auth/login', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify({
             username: username.value,
             password: password.value
         })
     });
+
     const data = await res.json();
+
     localStorage.setItem('token', data.token);
+
     location.href = 'dashboard.html';
 }
