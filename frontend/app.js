fdaa32f57bb32f474b3309e8f44e380cae1b573f
Wangziyi-1207 <whypnos3@gmail.com>
2026-06-15 19:40:34 +0900

    Refactor API URL and improve header formatting

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,17 @@
-const API = "http://localhost:3000/tasks";
+const API = "/tasks";
 
 async function loadTasks() {
     const res = await fetch(API, {
-        headers: { 'Authorization': localStorage.getItem('token') }
+        headers: {
+            Authorization: localStorage.getItem('token')
+        }
     });
+
     const tasks = await res.json();
 
     const list = document.getElementById('list');
     list.innerHTML = '';
+
     tasks.forEach(t => {
         const li = document.createElement('li');
         li.innerText = t.title;
