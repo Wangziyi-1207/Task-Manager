d72afe39c78315b2809003ea2a20f13aa2b32217
Wangziyi-1207 <whypnos3@gmail.com>
2026-06-15 20:08:34 +0900

    Update database connection to use path.join
    Refactor database path to use path.join for better portability.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.db');
+const path = require('path');
+
+const dbPath = path.join(__dirname, '../database.db');
+
+console.log('DB Path:', dbPath);
+
+const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
